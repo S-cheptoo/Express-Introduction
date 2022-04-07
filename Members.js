9d49ef152f3b857272418ce661d4c97cb3ce8dac
Sandra Cheptoo <cheptoosandra.t@gmail.com>
2022-04-07 11:57:39 +0200

    Creating and consuming static files

---
+++ Members.js
@@ -0,0 +1,21 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'johndoe@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Sandra Chep',
+        email: 'sandra@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'Mark M',
+        email: 'makiem@gmail.com',
+        status: 'inactive'
+    }
+];
+module.exports = members;
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const members = require('./Members');
+
+const app = express();
+
+
+//get all members
+app.get('/api/members', (req, res) =>
+    res.json(members)
+);
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+//Set a static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
