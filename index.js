609fe0ead25498eaa37eee929232bc01b31182e9
Sandra Cheptoo <cheptoosandra.t@gmail.com>
2022-04-07 15:00:52 +0200

    Create an item and oushing it to the array

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const members = require('./Members');
 const logger = require('./middleware/logger')
 
 const app = express();
@@ -8,22 +7,9 @@ const app = express();
 //Init middleware 
 // app.use(logger);
 
-//get all members
-app.get('/api/members', (req, res) =>
-    res.json(members)
-);
-
-//Get a single member
-app.get('/api/members/:id', (req,res) =>{
-    const found = members.some(member => member.id ===parseInt(req.params.id));
-
-    if (found){
-        //take the members array and filter out 
-        res.json(members.filter(member => member.id === parseInt(req.params.id))); 
-    } else {
-        res.status(400).json({ msg: `No member with the id of ${req.params.id}`});
-    }
-})
+// Body Parser Middleware 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
 
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -31,6 +17,9 @@ app.get('/api/members/:id', (req,res) =>{
 
 //Set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Members API Routes
+app.use('/api/members', require('./routes/api/members'));
 
 
 const PORT = process.env.PORT || 5000;
